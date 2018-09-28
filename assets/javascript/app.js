2dfd476c6cfc2a5441e9d1539aba49ddf8150d0b
CircuitSyn <circuitsyn@gmail.com>
2018-09-27 22:11:35 -0700

    adding back all code, files, and images to development branch

---
+++ assets/javascript/app.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+
+
+    //Background Image JS
+    
+    var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg'];
+    $('#background').css({'background-image': 'url(assets/images/' + images[Math.floor(Math.random() * images.length)] + ')'});
+    
+    });
+    
+    //End of Background Image JS
+       
+       // start of google map and geocode api calls and functions
+       // grab our search button ID
+       var submitForm = $('#search-button');
+       // create on click function to run when we click submit
+       submitForm.on("click", function(event) {
+       // Prevent page reload on submit
+       event.preventDefault();
+       // location varible
+       var location = $("#search-input").val();
+       
+       var queryUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyBoDweaPm-2OM397JbMV3n1L-WqHM6ABOM"
+       $.ajax({
+           url: queryUrl,
+           method: "GET"
+       }).then(function (response) {
+           // log full response
+           console.log(response);
+           // pull lat and lng
+           var lat = response.results[0].geometry.location.lat
+           var lng = response.results[0].geometry.location.lng
+           // log our lat and lng to the console
+           console.log(lat);
+           console.log(lng);
+           // pull formal formated address
+           var dest = response.results[0].formatted_address
+           // log our formal address
+           console.log(dest);
+           // push formated_address to #mainline div
+           $('#mainLine').text(dest);
+           // create a map variable
+             var map;
+             // function to create our map
+             function initMap() {
+                 // appending our #map div to our map variable 
+               map = new google.maps.Map(document.getElementById('map'), {
+                   // choose our location
+                 center: {lat: lat, lng: lng},
+                 // choose our zoom
+                 zoom: 8
+               });
+               // create marker variable 
+               var marker = new google.maps.Marker({
+                   // grab lat and lng for our positioning, same as our center
+               position:{lat: lat, lng: lng},
+               // put it on the same map
+               map:map,
+               })
+             }
+             // call initMap funciton
+             initMap();
+            //  $('#map').html(map);
+    
+            // end of google map and geocode api calls and functions
+            
+            //=========================================================================
+            
+            // Start of Events API - Meetup.com
+            var meetupAPI = '1e26b5231546533317352a49132a2';
+            
+            $.ajax({
+                url: 'https://api.meetup.com/2/open_events?key=' + meetupAPI + '&sign=true&photo-host=public&lat=' + lat + '&lon=' + lng + '&page=9',
+                dataType: 'jsonp',
+                // Function to call on success
+                success: function(data) {
+                console.log(data);
+                    
+                }
+            });
+            // End of Events API - Meetup.com
+            //==========================================================================
+    
+    
+    
+    
+       });
+       
+    
+       });
