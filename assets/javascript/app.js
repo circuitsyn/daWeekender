25a453ed2bce236ddeb7310a6a50efcc67ca813e
circuitsyn <circuitsyn@gmail.com>
2018-10-08 14:15:36 -0700

    ignore these blank changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,39 @@ $(document).ready(function () {
 
     //initializing firebase config
     firebase.initializeApp(config);
+    
+    // //========Firebase Sign in and Sign Out Code==========
+    // var provider = new firebase.auth.GoogleAuthProvider();
 
+    // function googleSignin() {
+    // firebase.auth()
+    
+    // .signInWithPopup(provider).then(function(result) {
+    //     var token = result.credential.accessToken;
+    //     var user = result.user;
+            
+    //     console.log(token)
+    //     console.log(user)
+    // }).catch(function(error) {
+    //     var errorCode = error.code;
+    //     var errorMessage = error.message;
+            
+    //     console.log(error.code)
+    //     console.log(error.message)
+    // });
+    // }
+
+    // function googleSignout() {
+    // firebase.auth().signOut()
+        
+    // .then(function() {
+    //     console.log('Signout Succesfull')
+    // }, function(error) {
+    //     console.log('Signout Failed')  
+    // });
+    // }
+
+    // //========End of login/logout code==============
     // ===========GLOBAL VARIABLES===============
     var lat = 0;
     var lng = 0;
